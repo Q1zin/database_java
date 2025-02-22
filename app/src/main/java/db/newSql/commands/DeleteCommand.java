0eff3765e6f5eab271d3911c8873e97c0750cef4
q1zin <q1zin@mail.ru>
2025-02-23 02:11:47 +0700

    add delete command

--- app/src/main/java/db/newSql/commands/DeleteCommand.java
+++ app/src/main/java/db/newSql/commands/DeleteCommand.java
@@ -1,9 +1,20 @@
 package db.newSql.commands;
 
 import db.newSql.dataBase.Database;
+import db.newSql.dataBase.Table;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @CommandName("DELETE")
 public class DeleteCommand extends AbstractCommand {
+    private String tableName;
+    private String conditionString;
+    private List<String> conditionList = new ArrayList<>();
+    List<Map<String, Object>> listToDeleted;
 
     public DeleteCommand(String sql) {
         super(sql);
@@ -11,7 +22,6 @@ public class DeleteCommand extends AbstractCommand {
 
     @Override
     public void execute(Database db) {
-        System.out.println("Ещё не сделал!");
         validate_sql();
         parse_data();
         validate_data(db);
@@ -19,17 +29,40 @@ public class DeleteCommand extends AbstractCommand {
     }
 
     private void validate_sql() {
+        Pattern tablePattern = Pattern.compile("^DELETE FROM ([a-zA-Z_][a-zA-Z0-9_]*) WHERE (.+)$");
+        Matcher tableMatcher = tablePattern.matcher(sql);
 
+        if (!tableMatcher.matches()) {
+            throw new IllegalArgumentException("Неверный формат команды!");
+        }
+
+        tableName = tableMatcher.group(1);
+        conditionString = tableMatcher.group(2);
     }
 
     private void parse_data() {
+        if (conditionString.isEmpty()) {
+            return;
+        }
 
+        Pattern conditionPattern = Pattern.compile("([a-zA-Z_][a-zA-Z0-9_]*)\\s*(>=|<=|!=|=|>|<)\\s*(\\\"[^\\\"]*\\\"|\\[(?:[^\\[\\]]*?)\\]|[a-zA-Z0-9_\\-]+)"); // мб когда-нибудь реализую OR, !=, <, ...
+        Matcher conditionMatcher = conditionPattern.matcher(conditionString);
+
+        while (conditionMatcher.find()) {
+            conditionList.add(conditionMatcher.group(1) + conditionMatcher.group(2) +  conditionMatcher.group(3));
+        }
     }
 
     private void validate_data(Database db) {
+        if (!db.containsTable(tableName)) {
+            throw new IllegalArgumentException("Таблицы с таким именем нету");
+        }
 
+        Table table = db.getTable(tableName);
+        listToDeleted = table.getWhereData(conditionList);
     }
-    private void do_request(Database db) {
 
+    private void do_request(Database db) {
+        db.getTable(tableName).getData().removeIf(listToDeleted::contains);
     }
 }
--- app/src/main/java/db/newSql/dataBase/Table.java
+++ app/src/main/java/db/newSql/dataBase/Table.java
@@ -2,9 +2,9 @@ package db.newSql.dataBase;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Table {
     private String name;
@@ -63,5 +63,55 @@ public class Table {
         }
 
         return minColumns == -1 ? 0 : minColumns + 1;
+    }
+
+    @JsonIgnore
+    public List<Map<String, Object>> getWhereData(List<String> conditionList) {
+        List<Map<String, Object>> newData = new ArrayList<>();
+
+        // пока хз, как быстрее всего стравнивать значения, но пусть пока будет так
+        for (Map<String, Object> row : data) {
+            boolean flag = true;
+            for (String condition : conditionList) {
+                boolean result = checkCondition(row, condition);
+                if (!result) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                Map<String, Object> rowCopy = new HashMap<>(row);
+                newData.add(rowCopy);
+            }
+        }
+
+        return newData;
+    }
+
+    private boolean checkCondition(Map<String, Object> row, String condition) {
+        Pattern pattern = Pattern.compile("([a-zA-Z_][a-zA-Z0-9_]*)(>=|<=|!=|=|>|<)(.+)");
+        Matcher matcher = pattern.matcher(condition);
+
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("Некорректное условие: " + condition);
+        }
+
+        Object fieldValue = row.get(matcher.group(1));
+        String operator = matcher.group(2);
+        String valueStr = matcher.group(3);
+
+        return compareValues(fieldValue, operator, valueStr);
+    }
+
+    private boolean compareValues(Object fieldValue, String operator, String valueStr) {
+        if (fieldValue == null) {
+            return false;
+        }
+
+        if (!operator.equals("=")) {
+            throw new IllegalArgumentException("Не реализована такая операция сравнения!");
+        }
+
+        return Objects.equals(fieldValue.toString(), valueStr);
     }
 }
